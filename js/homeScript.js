466acfb92f87a1ea697f6e06c7e2844f28dca666
theclayton <clayton@flaresoftware.com>
2019-11-13 22:56:08 -0700

    Timeline bubble style, horizonal scrolling with mouse wheel.

--- js/homeScript.js
+++ js/homeScript.js
@@ -1,7 +1,32 @@
+// Horizontal Timeline Scrolling
+(function() {
+    function scrollHorizontally(e) {
+        e = window.event || e;
+        var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+        document.getElementById('timeline').scrollLeft += (delta*40); // Multiplied by 40
+
+        if (document.getElementById('timeline').scrollLeft > 0) {
+            e.preventDefault();
+        }
+
+    }
+    if (document.getElementById('timeline').addEventListener) {
+        // IE9, Chrome, Safari, Opera
+        document.getElementById('timeline').addEventListener("mousewheel", scrollHorizontally, false);
+        // Firefox
+        document.getElementById('timeline').addEventListener("DOMMouseScroll", scrollHorizontally, false);
+    } else {
+        // IE 6/7/8
+        document.getElementById('timeline').attachEvent("onmousewheel", scrollHorizontally);
+    }
+})();
+
+
+
+
+
+// jQuery
 $(function(){
-
-
-
 
 
 });
