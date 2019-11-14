190028fc343412ea92020386b059a34bdff7c16b
Clayton Ward <clayton@flaresoftware.com>
2019-11-14 12:29:34 -0700

    Added timeline events

--- js/homeScript.js
+++ js/homeScript.js
@@ -1,11 +1,12 @@
 // Horizontal Timeline Scrolling
 (function() {
     function scrollHorizontally(e) {
+        const timelineWidth = 945;
         e = window.event || e;
         var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
         document.getElementById('timeline').scrollLeft += (delta*40); // Multiplied by 40
 
-        if (document.getElementById('timeline').scrollLeft > 0) {
+        if ((document.getElementById('timeline').scrollLeft > 0) && (document.getElementById('timeline').scrollLeft < timelineWidth)) {
             e.preventDefault();
         }
 
