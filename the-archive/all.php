ae88d792eab6696068ea0ac4ea70ef4766c80ea2
Clayton Ward <clayton@flaresoftware.com>
2019-11-19 12:55:41 -0700

    Experience menu, created the archive, minor tweaks

---
+++ the-archive/all.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>The Archive: All Projects, Clayton Ward</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" type="text/css" href="../style/font.css">
+<link rel="stylesheet" type="text/css" href="../style/home.css">
+
+<!-- This site uses JQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script>
+   window.jQuery || document.write('<script src="js/jquery/jquery-3.4.1.min.js"><\/script>');
+</script>
+
+</head> <!-- emd head -->
+
+
+<body>
+
+  <div class="aMenu">
+  <?php
+  include 'archivemenu.php';
+  ?>
+  </div>
+
+
+  <div id="footerSection">
+  </div> <!-- end footerSection div -->
+
+
+  <!-- javascript -->
+  <!-- <script src="js/homeScript.js"></script>  -->
+
+  <!-- end javascript -->
+
+</body>
+</html>
---
+++ the-archive/archive-template.php
@@ -0,0 +1,13 @@
+<?php
+echo '
+  // side archive tree menu, header, footer, content style
+  <div class="experienceSquares">
+    <a class="eSquare" href=""><img class="eSquareImg" src="a.png" alt="" height="100" width="100"><h3>Mobile</h3></a>
+    <a class="eSquare" href=""><img class="eSquareImg" src="a.png" alt="" height="100" width="100"><h3>Web</h3></a>
+    <a class="eSquare" href=""><img class="eSquareImg" src="a.png" alt="" height="100" width="100"><h3>Embedded</h3></a>
+    <a class="eSquare" href=""><img class="eSquareImg" src="a.png" alt="" height="100" width="100"><h3>Linux</h3></a>
+    <a class="eSquare" href=""><img class="eSquareImg" src="a.png" alt="" height="100" width="100"><h3>Graphic Design</h3></a>
+    <a class="eSquare" href="https://github.com/theclayton/"><img class="eSquareImg" src="a.png" alt="" height="100" width="100"><h3>Github</h3></a>
+  </div>
+  ';
+?>
