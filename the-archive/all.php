b8b82ffa9bf3597b715055e5c9e277365eff9957
Clayton Ward <clayton@flaresoftware.com>
2019-11-21 13:06:16 -0700

    Updated archive menu

--- the-archive/all.php
+++ the-archive/all.php
@@ -37,33 +37,73 @@
 
       <li id="cate-1"><span class="caret">iOS Development</span>
         <ul class="aNested">
-          <li id="cate-1-proj-1"><a href="">App 1</a></li>
-          <li id="cate-1-proj-2"><a href="">App 2</a></li>
-          <li id="cate-1-proj-3"><a href="">App 3</a></li>
-          <li id="cate-1-proj-4"><a href="">App 4</a></li>
-          <li id="cate-1-proj-5"><a href="">App 5</a></li>
-          <li id="cate-1-proj-6"><a href="">App 6</a></li>
-          <li id="cate-1-proj-7"><a href="">App 7</a></li>
-          <li id="cate-1-proj-8"><a href="">App 8</a></li>
-          <li id="cate-1-proj-9"><a href="">App 9</a></li>
-          <li id="cate-1-proj-10"><a href="">App 10</a></li>
-          <li id="cate-1-proj-11"><a href="">App 11</a></li>
+          <li id="cate-1-proj-1"><a href="">Boondoggle - December 16, 2010</a></li>
+          <li id="cate-1-proj-2"><a href="">Clash ots - Mid-2011</a></li>
+          <li id="cate-1-proj-3"><a href="">City Sky - October 2011</a></li>
+          <li id="cate-1-proj-4"><a href="">Xellic - January 25, 2012</a></li>
+          <li id="cate-1-proj-5"><a href="">Xellix II - May 23, 2012</a></li>
+          <li id="cate-1-proj-6"><a href="">Scoubidou - March 18, 2013</a></li>
+          <li id="cate-1-proj-7"><a href="">Forte - Mid 2013</a></li>
+          <li id="cate-1-proj-8"><a href="">Paracord 3D - September 2013</a></li>
+          <li id="cate-1-proj-9"><a href="">Scoubi - December 13, 2013</a></li>
+          <li id="cate-1-proj-10"><a href="">Scroller - March 13, 2014</a></li>
+          <li id="cate-1-proj-11"><a href="">Fur Elise - April 5, 2014</a></li>
+          <li id="cate-1-proj-12"><a href="">Scroller MXL - </a></li>
+          <li id="cate-1-proj-13"><a href="">Piano Scales - </a></li>
+          <li id="cate-1-proj-14"><a href="">Improv piano - </a></li>
+          <li id="cate-1-proj-15"><a href="">Music Library - </a></li>
         </ul>
       </li>
 
-    <li><span class="caret">Android Development</span>
+    <li id="cate-2"><span class="caret">Android Development</span>
       <ul class="aNested">
-        <li id="cate-2-proj-1"><a href="">App 1</a></li>
-        <li id="cate-2-proj-2"><a href="">App 2</a></li>
+        <li id="cate-2-proj-1"><a href="">Paracord</a></li>
+        <li id="cate-2-proj-2"><a href="">Scoubi-Doggle</a></li>
+        <li id="cate-2-proj-2"><a href="">Improv Piano</a></li>
       </ul>
     </li>
 
-      <li><span class="caret">Web Development</span>
-        <ul class="aNested">
-          <li id="cate-3-proj-1"><a href="">App 1</a></li>
-          <li id="cate-3-proj-2"><a href="">App 2</a></li>
-        </ul>
-      </li>
+    <li id="cate-3"><span class="caret">Web Development</span>
+      <ul class="aNested">
+        <li id="cate-3-proj-1"><a href="">This site Resume</a></li>
+        <li id="cate-3-proj-2"><a href="">MTC More</a></li>
+        <li id="cate-3-proj-3"><a href="">MLP?</a></li>
+        <li id="cate-3-proj-4"><a href="">Scoubi-doggle?</a></li>
+      </ul>
+    </li>
+
+    <li id="cate-4"><span class="caret">Embedded Systems</span>
+      <ul class="aNested">
+        <li id="cate-4-proj-1"><a href="">3 X 3 display</a></li>
+      </ul>
+    </li>
+
+    <li id="cate-5"><span class="caret">Linux Development</span>
+      <ul class="aNested">
+        <li id="cate-5-proj-1"><a href="">My Distro</a></li>
+        <li id="cate-5-proj-2"><a href="">Some useful program</a></li>
+        <li id="cate-5-proj-3"><a href="">Some useful shell script</a></li>
+      </ul>
+    </li>
+
+    <li id="cate-6"><span class="caret">Ruby on Rails</span>
+      <ul class="aNested">
+        <li id="cate-6-proj-1"><a href="">My program</a></li>
+      </ul>
+    </li>
+
+    <li id="cate-7"><span class="caret">Windows Desktop</span>
+      <ul class="aNested">
+        <li id="cate-7-proj-1"><a href="">My program</a></li>
+      </ul>
+    </li>
+
+    <li id="cate-7"><span class="caret">Small Misc Projects</span>
+      <ul class="aNested">
+        <li id="cate-7-proj-1"><a href="">My program (school, practice, fun)</a></li>
+      </ul>
+    </li>
+
 
     </ul>
   </li>
